84916a23459bf827bdea53243f4dad95e08f37b8
IchsanGhofur <a710230011@student.ums.ac.id>
2024-06-12 10:31:21 +0700

    Update Variabel.py

--- Day 3/Variabel.py
+++ Day 3/Variabel.py
@@ -1,8 +1,14 @@
+from math import pi
 Nama = input(str("Nama saya adalah: "))
 asal = input(str("Asal saya: "))
 umur = input(("Umur saya saat ini: "))
 umur1 = int(umur)
 umur_lama = int(umur1 - 10) 
-umur_depan = int(umur1 + 10)
+umur_depan = int(umur1 + 17)
 print("Umur saya 10 tahun yang lalu adalah ", umur_lama)
-print("10 tahun lagi umur saya adalah ", umur_depan)
+print("17 tahun lagi umur saya adalah ", umur_depan)
+
+A = input(("masukkan jari-jari: "))
+A1 = int(A)
+Luas = (A1 * A1 * pi )
+print("Luas lingkaran dengan panjang jari jari {} adalah {}".format(A, Luas))
