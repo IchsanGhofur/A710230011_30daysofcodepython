894b7ce5f9bac067bd1b0118967445bd12261523
IchsanGhofur <a710230011@student.ums.ac.id>
2024-06-15 20:21:00 +0700

    hari ke 7

--- Day 7/object.py
+++ Day 7/object.py
@@ -1,6 +1,7 @@
 class persegi:
     def __init__(self, sisi):
         self.sisi = sisi
+        
     def Luas(self, sisi):
         luas = (sisi * sisi)
         print("Luas persegi adalah ", luas)
