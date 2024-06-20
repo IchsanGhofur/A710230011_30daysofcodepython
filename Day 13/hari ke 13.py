dce709340d661552191e9e62f9fb61877b6e517f
IchsanGhofur <a710230011@student.ums.ac.id>
2024-06-20 20:58:12 +0700

    hari ke 13

---
+++ Day 13/hari ke 13.py
@@ -0,0 +1,14 @@
+kata = 'Pendidikan Teknik Informatika'
+nama_depan = input('Nama depan :')
+nama_belakang = input('Nama belakang :')
+nim = input('Nim :')
+fakultas = 'Fakultas Pendidikan dan Ilmu Pendidikan'
+kampus = input('kampus :')
+nomor = input('nomor: ')
+print(kata.center(50))
+print('Nama: {} {}'.format(nama_depan, nama_belakang))
+print('Nim: ', nim)
+print(nomor.center(50))
+print(fakultas)
+kapital = kampus.upper()
+print(kapital.center(50))
