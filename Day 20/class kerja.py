91ff67b1df7c131b7aa9105f982ecdce29fba2d6
IchsanGhofur <a710230011@student.ums.ac.id>
2024-06-27 20:19:59 +0700

    hari ke 20

---
+++ Day 20/class kerja.py
@@ -0,0 +1,25 @@
+class Pekerja:
+    def __init__(self, name, age, salary):
+        self.__name = name
+        self.__age = age
+        self.__salary = salary
+    def set_name(self, name):
+        self.__name = name
+    def set_age(self, age):
+        self.__age = age
+    def set_salary(self, salary):
+        self.__salary = salary
+    def get_name(self):
+        return self.__name
+    def get_age(self):
+        return self.__age
+    def get_salary(self):
+        return self.__salary
+karyawan = Pekerja("Parmin", 24, 5000000)
+print(karyawan.get_name())
+print(karyawan.get_age())
+print(karyawan.get_salary())
+karyawan.set_name("Fatih")
+karyawan.set_salary(5500000)
+print(karyawan.get_name())
+print(karyawan.get_salary())
